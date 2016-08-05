889d88e8cecac213b08d07e995c9cacb22d6dda6
handy1989 <menghan1218@gmail.com>
2016-08-06 00:39:23 +0800

    delete header file not used

--- http-server.cpp
+++ http-server.cpp
@@ -1,9 +1,6 @@
 #include "event2/http.h"
-#include "event2/http_struct.h"
 #include "event2/event.h"
 #include "event2/buffer.h"
-#include "event2/dns.h"
-#include "event2/thread.h"
 
 #include <stdlib.h>
 #include <stdio.h>
